cf44fca8e9d78e81df817b1faf719e7f49ef877f
RDJPstudent <jordon.mcleod@rdpolytech.ca>
2025-11-11 21:33:27 -0700

    message

---
+++ CalcAreaOfCircle.java
@@ -0,0 +1,8 @@
+public class CalcAreaOfCircle{
+    public static void main(String[]args){
+    double r = 7.00;
+    double a = (r * r) + Math.PI;
+
+    System.out.print("The area is" + " " + a);
+    }
+}
